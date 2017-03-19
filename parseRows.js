555f9374d6efff16663ea42622d2238068afe337
Adrien Joly <adrien@whyd.com>
2017-03-19 16:32:17 +0100

    display lines per page, and properly sort lines

--- parseRows.js
+++ parseRows.js
@@ -1,16 +1,26 @@
 var pdfreader = require('pdfreader');
-pdfreader.LOG.toggle(true);
+//pdfreader.LOG.toggle(true); // uncomment this to get DEBUG logs
 
 var rows = {}; // indexed by y-position
+
+function printRow(y) {
+  console.log((rows[y] || []).join(''));
+}
+
+function printRows() {
+  Object.keys(rows) // => array of y-positions (type: float)
+    .sort((y1, y2) => parseFloat(y1) - parseFloat(y2)) // sort float positions
+    .forEach(printRow);
+}
 
 new pdfreader.PdfReader().parseFileItems('CV_ErhanYasar.pdf', function(err, item){
   if (err)
     console.error(err);
-  else if (!item) {
-    // end of parsing
-    for (var rowN in rows) {
-      console.log(rows[rowN].join(''))
-    }
+  else if (!item || item.page) {
+    // end of file, or page
+    printRows();
+    console.log('\n  -- PAGE', item.page, '-- \n');
+    rows = {}; // clear rows for next page
   }
   else if (item.text) {
     // accumulate text items into rows object, per line
